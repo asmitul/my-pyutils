1c7bd75456dbf1259a3066956cddc39adbb49b6c
asmitul <asmitullah@gmail.com>
2024-12-09 11:12:05 +0300

    Enhance module initialization by importing str_to_number and updating docstring formatting in __init__.py

--- src/my_pyutils/__init__.py
+++ src/my_pyutils/__init__.py
@@ -1,4 +1,8 @@
-"""`my_pyutils `: my python utils"""
+"""my_pyutils: my python utils"""
+
+from .str_to_number import str_to_number
 
 __author__ = "asmitul <asmitul@hotmail.com>"
 __version__ = "0.0.1"
+
+__all__ = ["str_to_number"]
